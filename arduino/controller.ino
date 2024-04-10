cbee649870dc3c0990d16a3a81907301336947d7
Pablo Ferreira <pablomartinferre@gmail.com>
2024-04-09 21:05:19 -0300

    Agregado de servos para ejes X e Y

--- arduino/controller.ino
+++ arduino/controller.ino
@@ -1,8 +1,19 @@
+#include <Servo.h>
+
+// Pin map
+//
+
 #define LDR0 A0
 #define LDR1 A1
 #define LDR2 A2
 #define LDR3 A3
 #define LDR4 A4
+
+#define SERV_X 5
+#define SERV_Y 6
+
+// Magic numbers
+//
 
 #define MAX_VAL 1023
 
@@ -23,20 +34,32 @@ typedef struct
 
 int ldr_array[5];
 Point2D sun_pos;
+Servo servX;
+Servo servY;
 
 void setup()
 {
 	Serial.begin(9600);
+	servX.attach(SERV_X);
+	servY.attach(SERV_Y);
 }
 
 void loop()
 {
 	readSensors();
 	calculateSunPosition();
+	positionServos();
 
+	// Para debugging
 	Serial.print(sun_pos.x);
 	Serial.print("\t");
-	Serial.println(sun_pos.y);
+	Serial.print(sun_pos.y);
+	Serial.print("\t");
+	Serial.print(sun_pos.x * 180 / MAX_VAL); // TODO
+	Serial.print("d");
+	Serial.print("\t");
+	Serial.print(sun_pos.y * 180 / MAX_VAL); // TODO
+	Serial.println("d");
 
 	delay(1000);
 }
@@ -54,4 +77,10 @@ void calculateSunPosition()
 {
 	sun_pos.x = (ldr_array[1] - BASE1) - (ldr_array[3] - BASE3);
 	sun_pos.y = (ldr_array[2] - BASE2) - (ldr_array[4] - BASE4);
+}
+
+void positionServos()
+{
+	servX.write(sun_pos.x * 180 / MAX_VAL); // TODO transformar coordenadas
+	servY.write(sun_pos.y * 180 / MAX_VAL); // TODO
 }
