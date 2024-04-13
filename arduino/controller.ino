485496528029c422f67ba0f4b52f26b0cb0e3b96
Pablo Ferreira <74799786+Dr-Pe@users.noreply.github.com>
2024-04-12 23:00:13 -0300

    Coordenadas cartesianas a esfericas y movimiento de los servos

--- arduino/controller.ino
+++ arduino/controller.ino
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <Servo.h>
 
 // Pin map
@@ -9,8 +10,8 @@
 #define LDR3 A3
 #define LDR4 A4
 
-#define SERV_X 5
-#define SERV_Y 6
+#define SERV_THETA 5
+#define SERV_ANTI_PHI 6
 
 // Magic numbers
 //
@@ -28,39 +29,55 @@
 
 typedef struct
 {
-	int x;
-	int y;
-} Point2D;
+	double x;
+	double y;
+  	double z;
+} Cartesian;
+
+typedef struct
+{
+	double theta;
+	double phi;
+} Spherical;
 
 int ldr_array[5];
-Point2D sun_pos;
-Servo servX;
-Servo servY;
+Cartesian sun_pos_plane;
+Spherical sun_pos_sphere;
+int theta_deg;
+int phi_deg;
+int flag = 0;
+
+Servo servTheta;
+Servo servPhi;
 
 void setup()
 {
 	Serial.begin(9600);
-	servX.attach(SERV_X);
-	servY.attach(SERV_Y);
+	servTheta.attach(SERV_THETA);
+	servPhi.attach(SERV_ANTI_PHI);
 }
 
 void loop()
 {
 	readSensors();
 	calculateSunPosition();
+	calculateSphericalCoordinates();
+	calculateAngles();
 	positionServos();
 
 	// Para debugging
-	Serial.print(sun_pos.x);
-	Serial.print("\t");
-	Serial.print(sun_pos.y);
-	Serial.print("\t");
-	Serial.print(sun_pos.x * 180 / MAX_VAL); // TODO
-	Serial.print("d");
-	Serial.print("\t");
-	Serial.print(sun_pos.y * 180 / MAX_VAL); // TODO
+	Serial.print(sun_pos_plane.x);
+	Serial.print("x\t");
+	Serial.print(sun_pos_plane.y);
+	Serial.print("y\t");
+	Serial.print(sun_pos_plane.z);
+	Serial.print("z\t");
+	Serial.print(theta_deg); // TODO
+	Serial.print("d\t");
+	Serial.print(phi_deg); // TODO
 	Serial.println("d");
 
+	flag = 1;
 	delay(1000);
 }
 
@@ -75,12 +92,36 @@ void readSensors()
 
 void calculateSunPosition()
 {
-	sun_pos.x = (ldr_array[1] - BASE1) - (ldr_array[3] - BASE3);
-	sun_pos.y = (ldr_array[2] - BASE2) - (ldr_array[4] - BASE4);
+	sun_pos_plane.x = ldr_array[1] - ldr_array[3]; // TODO evaluar posibilidad de resto haciendo lio
+	sun_pos_plane.y = ldr_array[2] - ldr_array[4];
+  	sun_pos_plane.z = ldr_array[0];
+}
+
+void calculateSphericalCoordinates()
+{
+	sun_pos_sphere.theta = atan(sqrt(sun_pos_plane.x * sun_pos_plane.x + sun_pos_plane.y * sun_pos_plane.y) / sun_pos_plane.z);
+	if(sun_pos_plane.x > 0 && sun_pos_plane.y > 0) // 1Q
+	{
+		sun_pos_sphere.phi = atan(sun_pos_plane.y / sun_pos_plane.x);
+	}
+	else if(sun_pos_plane.x < 0) // 2Q y 3Q
+	{
+		sun_pos_sphere.phi = atan(sun_pos_plane.y / sun_pos_plane.x) + M_PI;
+	}
+	else // 4Q
+	{
+		sun_pos_sphere.phi = 2 * M_PI + atan(sun_pos_plane.y / sun_pos_plane.x);
+	}
+}
+
+void calculateAngles()
+{
+	theta_deg = sun_pos_sphere.theta * 180 / M_PI;
+	phi_deg = sun_pos_sphere.phi * 180 / M_PI;
 }
 
 void positionServos()
 {
-	servX.write(sun_pos.x * 180 / MAX_VAL); // TODO transformar coordenadas
-	servY.write(sun_pos.y * 180 / MAX_VAL); // TODO
+	servTheta.write(theta_deg + flag * (theta_deg - servTheta.read()));
+	servPhi.write(phi_deg + flag * (phi_deg - servPhi.read()));
 }
