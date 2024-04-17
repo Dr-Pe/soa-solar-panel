0e0d926e56768e2859fc18b121684c3d79eeaaef
Dr-Pe <pablomartinferre@gmail.com>
2024-04-16 22:40:57 -0300

    Comienzo programa mod-5-pos-3-sen y gitignore

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -4,115 +4,104 @@
 // Pin map
 //
 
-#define LDR0 A0
-#define LDR1 A1
-#define LDR2 A2
-#define LDR3 A3
-#define LDR4 A4
+#define LDR_Z A0
+#define LDR_X A1
+#define LDR_Y A2
 
-#define SERV_THETA 5
-#define SERV_ANTI_PHI 6
-
-// Magic numbers
-//
-
-#define MAX_VAL 1023
-
-#define BASE0 1000
-#define BASE1 400
-#define BASE2 400
-#define BASE3 400
-#define BASE4 400
+#define SERV_AZ 5
+#define SERV_ALT 6
 
 // C++ code
 //
 
+enum MachineState
+{
+	SETUP,
+	ON_Q1_Q3_AXIS,
+	ON_Q2_Q4_AXIS,
+	ON_Q0,
+	ON_Q1,
+	ON_Q2,
+	ON_Q3,
+	ON_Q4
+};
+
+enum Event
+{
+	PASS
+}
+
 typedef struct
 {
-  double x;
-  double y;
-  double z;
-} Cartesian;
+	int q0;
+	int q1;
+	int q2;
+	int q3;
+	int q4;
+} SkyState;
 
 typedef struct
 {
-  double theta;
-  double phi;
-} Spherical;
+	int z;
+	int x;
+	int y;
+} Scanner;
 
-int ldr_array[5];
-Cartesian sun_pos_plane;
-Spherical sun_pos_sphere;
-int theta_deg;
-int phi_deg;
-int flag = 0;
+Servo serv_az;
+Servo serv_alt;
+MachineState machine_state;
+SkyState sky_state;
+Scanner scanner;
 
-Servo servTheta;
-Servo servPhi;
-
-void setup() {
-  Serial.begin(9600);
-  servTheta.attach(SERV_THETA);
-  servPhi.attach(SERV_ANTI_PHI);
+void setup()
+{
+	state = SETUP;
+	Serial.begin(9600);
+	serv_az.attach(SERV_AZ);
+	serv_alt.attach(SERV_ALT);
 }
 
-void loop() {
-  readSensors();
-  calculateSunPosition();
-  calculateSphericalCoordinates();
-  calculateAngles();
-  positionServos();
+void loop()
+{
+	Event event = get_event();
 
-  // Para debugging
-  Serial.print(sun_pos_plane.x);
-  Serial.print("x\t");
-  Serial.print(sun_pos_plane.y);
-  Serial.print("y\t");
-  Serial.print(sun_pos_plane.z);
-  Serial.print("z\t");
-  Serial.print(theta_deg);  // TODO
-  Serial.print("d\t");
-  Serial.print(phi_deg);  // TODO
-  Serial.println("d");
+	// Para debugging
+	Serial.print(sun_pos_plane.x);
+	Serial.print("x\t");
+	Serial.print(sun_pos_plane.y);
+	Serial.print("y\t");
+	Serial.print(sun_pos_plane.z);
+	Serial.print("z\t");
+	Serial.print(theta_deg); // TODO
+	Serial.print("d\t");
+	Serial.print(phi_deg); // TODO
+	Serial.println("d");
 
-  flag = 1;
-  delay(1000);
+	delay(1000);
 }
 
-void readSensors() {
-  ldr_array[0] = analogRead(LDR0);
-  ldr_array[1] = analogRead(LDR1);
-  ldr_array[2] = analogRead(LDR2);
-  ldr_array[3] = analogRead(LDR3);
-  ldr_array[4] = analogRead(LDR4);
-}
+// Captura de eventos
+//
 
-void calculateSunPosition() {
-  sun_pos_plane.x = ldr_array[1] - ldr_array[3];  // TODO evaluar posibilidad de resto haciendo lio
-  sun_pos_plane.y = ldr_array[2] - ldr_array[4];
-  sun_pos_plane.z = ldr_array[0];
-}
+Event get_event()
+{
+	scanner.z = readSensor(LDR_Z);
+	scanner.x = readSensor(LDR_X);
+	scanner.y = readSensor(LDR_Y);
 
-void calculateSphericalCoordinates() {
-  sun_pos_sphere.theta = atan(sqrt(sun_pos_plane.x * sun_pos_plane.x + sun_pos_plane.y * sun_pos_plane.y) / sun_pos_plane.z);
-  if (sun_pos_plane.x > 0 && sun_pos_plane.y > 0)  // 1Q
-  {
-    sun_pos_sphere.phi = atan(sun_pos_plane.y / sun_pos_plane.x);
-  } else if (sun_pos_plane.x < 0)  // 2Q y 3Q
-  {
-    sun_pos_sphere.phi = atan(sun_pos_plane.y / sun_pos_plane.x) + M_PI;
-  } else  // 4Q
-  {
-    sun_pos_sphere.phi = 2 * M_PI + atan(sun_pos_plane.y / sun_pos_plane.x);
-  }
-}
+	sky_state.q0 = scanner.z;
+	if (serv_az.read() == 0)
+	{
+		sky_state.q1 = scanner.x;
+		sky_state.q3 = scanner.y;
+		return PASS;
+	}
+	else // serv_az.read() == 90
+	{
+		sky_state.q2 = scanner.x;
+		sky_state.q4 = scanner.y;
+		// TODO: Ver qué estado Qn_IS_BIGGEST devolver
+	}
 
-void calculateAngles() {
-  theta_deg = sun_pos_sphere.theta * 180 / M_PI;
-  phi_deg = sun_pos_sphere.phi * 180 / M_PI;
-}
-
-void positionServos() {
-  servTheta.write(theta_deg + flag * (theta_deg - servTheta.read()));
-  servPhi.write(phi_deg + flag * (phi_deg - servPhi.read()));
+	// TODO: Timer
 }
