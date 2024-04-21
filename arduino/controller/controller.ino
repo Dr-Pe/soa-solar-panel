ef728041f2954523e12bf210c62e676abd428b6a
Dr-Pe <pablomartinferre@gmail.com>
2024-04-21 14:39:44 -0300

    Refactoreo

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -1,7 +1,6 @@
-#include <math.h>
 #include <Servo.h>
 
-#define MAX_TIME
+#define MAX_TIME 1000
 
 // Pin map
 //
@@ -14,26 +13,7 @@
 #define SERV_AZ 5
 #define SERV_ALT 6
 
-// Matrix
-//
-
-#define NUM_OF_STATES 8
-#define NUM_OF_EVENTS 7
-
-typedef void (*transition)();
-transition state_table[NUM_OF_STATES][NUM_OF_EVENTS] =
-	{
-		{aim_to_Q2_Q4_AXIS, error, error, error, error, error, error},		   // state ON_Q1_Q3_AXIS
-		{error, aim_to_Q0, aim_to_Q1, aim_to_Q2, aim_to_Q3, aim_to_Q4, error}, // state ON_Q2_Q4_AXIS
-		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q0
-		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q1
-		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q2
-		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q3
-		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS}		   // state ON_Q4
-
-};
-
-// C++ code
+// Data types
 //
 
 enum machinestate
@@ -56,7 +36,7 @@ enum event
 	Q2_IS_BIGGEST,
 	Q3_IS_BIGGEST,
 	Q4_IS_BIGGEST,
-	TIMER,
+	WAKE_UP,
 } new_event;
 
 struct skystate
@@ -80,6 +60,42 @@ Servo serv_alt;
 unsigned long time_from;
 unsigned long now;
 
+// Prototypes
+//
+
+event biggest_qn();
+event get_event();
+void aim_to_Q1_Q3_AXIS();
+void aim_to_Q2_Q4_AXIS();
+void aim_to_Q0();
+void aim_to_Q1();
+void aim_to_Q2();
+void aim_to_Q3();
+void aim_to_Q4();
+void error();
+
+// Matrix
+//
+
+#define NUM_OF_STATES 8
+#define NUM_OF_EVENTS 7
+
+typedef void (*transition)();
+transition state_table[NUM_OF_STATES][NUM_OF_EVENTS] =
+	{
+		{aim_to_Q2_Q4_AXIS, error, error, error, error, error, error},		   // state ON_Q1_Q3_AXIS
+		{error, aim_to_Q0, aim_to_Q1, aim_to_Q2, aim_to_Q3, aim_to_Q4, error}, // state ON_Q2_Q4_AXIS
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q0
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q1
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q2
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q3
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS}		   // state ON_Q4
+
+};
+
+// Main
+//
+
 void setup()
 {
 	machine_state = SETUP;
@@ -100,51 +116,11 @@ void loop()
 	new_event = get_event();
 	state_table[machine_state][new_event]();
 
-	// Para debugging
-	Serial.print(sun_pos_plane.x);
-	Serial.print("x\t");
-	Serial.print(sun_pos_plane.y);
-	Serial.print("y\t");
-	Serial.print(sun_pos_plane.z);
-	Serial.print("z\t");
-	Serial.print(theta_deg); // TODO
-	Serial.print("d\t");
-	Serial.print(phi_deg); // TODO
-	Serial.println("d");
-
 	delay(1000);
 }
 
 // Captura de eventos
 //
-
-event get_event()
-{
-	if (digitalRead(LED) == LOW)
-	{
-		now = millis();
-		if (now - time_from < MAX_TIME)
-			return TIMER;
-	}
-
-	array_of_sensors.z = readSensor(LDR_Z);
-	array_of_sensors.x = readSensor(LDR_X);
-	array_of_sensors.y = readSensor(LDR_Y);
-
-	sky_state.q0 = array_of_sensors.z;
-	if (serv_az.read() == 0)
-	{
-		sky_state.q1 = array_of_sensors.x;
-		sky_state.q3 = array_of_sensors.y;
-		return CONTINUE;
-	}
-	else // serv_az.read() == 90
-	{
-		sky_state.q2 = array_of_sensors.x;
-		sky_state.q4 = array_of_sensors.y;
-		return biggest_qn();
-	}
-}
 
 event biggest_qn()
 {
@@ -163,13 +139,41 @@ event biggest_qn()
 	if (max_value < sky_state.q3)
 	{
 		max_q = Q3_IS_BIGGEST;
-		max_value = sky_state.q3
+		max_value = sky_state.q3;
 	}
 	if (max_value < sky_state.q4)
 	{
 		max_q = Q4_IS_BIGGEST;
 	}
 	return max_q;
+}
+
+event get_event()
+{
+	if (digitalRead(LED) == LOW)
+	{
+		now = millis();
+		if (now - time_from < MAX_TIME)
+			return WAKE_UP;
+	}
+
+	array_of_sensors.z = analogRead(LDR_Z);
+	array_of_sensors.x = analogRead(LDR_X);
+	array_of_sensors.y = analogRead(LDR_Y);
+
+	sky_state.q0 = array_of_sensors.z;
+	if (serv_az.read() == 0)
+	{
+		sky_state.q1 = array_of_sensors.x;
+		sky_state.q3 = array_of_sensors.y;
+		return CONTINUE;
+	}
+	else // serv_az.read() == 90
+	{
+		sky_state.q2 = array_of_sensors.x;
+		sky_state.q4 = array_of_sensors.y;
+		return biggest_qn();
+	}
 }
 
 // Apuntado
@@ -179,6 +183,7 @@ void aim_to_Q1_Q3_AXIS()
 {
 	serv_az.write(0);
 	serv_alt.write(0);
+	digitalWrite(LED, HIGH);
 	machine_state = ON_Q1_Q3_AXIS;
 }
 
@@ -193,6 +198,7 @@ void aim_to_Q0()
 {
 	serv_az.write(0);
 	serv_alt.write(0);
+	digitalWrite(LED, LOW);
 	machine_state = ON_Q0;
 }
 
@@ -200,6 +206,7 @@ void aim_to_Q1()
 {
 	serv_az.write(0);
 	serv_alt.write(45);
+	digitalWrite(LED, LOW);
 	machine_state = ON_Q1;
 }
 
@@ -207,6 +214,7 @@ void aim_to_Q2()
 {
 	serv_az.write(90);
 	serv_alt.write(135);
+	digitalWrite(LED, LOW);
 	machine_state = ON_Q2;
 }
 
@@ -214,6 +222,7 @@ void aim_to_Q3()
 {
 	serv_az.write(0);
 	serv_alt.write(135);
+	digitalWrite(LED, LOW);
 	machine_state = ON_Q3;
 }
 
@@ -221,11 +230,13 @@ void aim_to_Q4()
 {
 	serv_az.write(90);
 	serv_alt.write(45);
+	digitalWrite(LED, LOW);
 	machine_state = ON_Q4;
 }
 
 // Error
 //
+
 void error()
 {
 	return;
