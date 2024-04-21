f97ac330343f8f1cbf487c749f4c46430a8967d7
Dr-Pe <pablomartinferre@gmail.com>
2024-04-21 13:17:05 -0300

    Implementacion de maquina de estados como matriz

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <Servo.h>
 
+#define MAX_TIME
+
 // Pin map
 //
 
@@ -8,13 +10,33 @@
 #define LDR_X A1
 #define LDR_Y A2
 
+#define LED 0
 #define SERV_AZ 5
 #define SERV_ALT 6
+
+// Matrix
+//
+
+#define NUM_OF_STATES 8
+#define NUM_OF_EVENTS 7
+
+typedef void (*transition)();
+transition state_table[NUM_OF_STATES][NUM_OF_EVENTS] =
+	{
+		{aim_to_Q2_Q4_AXIS, error, error, error, error, error, error},		   // state ON_Q1_Q3_AXIS
+		{error, aim_to_Q0, aim_to_Q1, aim_to_Q2, aim_to_Q3, aim_to_Q4, error}, // state ON_Q2_Q4_AXIS
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q0
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q1
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q2
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		   // state ON_Q3
+		{error, error, error, error, error, error, aim_to_Q1_Q3_AXIS}		   // state ON_Q4
+
+};
 
 // C++ code
 //
 
-enum MachineState
+enum machinestate
 {
 	SETUP,
 	ON_Q1_Q3_AXIS,
@@ -23,47 +45,60 @@ enum MachineState
 	ON_Q1,
 	ON_Q2,
 	ON_Q3,
-	ON_Q4
-};
+	ON_Q4,
+} machine_state;
 
-enum Event
+enum event
 {
-	PASS
-}
+	CONTINUE,
+	Q0_IS_BIGGEST,
+	Q1_IS_BIGGEST,
+	Q2_IS_BIGGEST,
+	Q3_IS_BIGGEST,
+	Q4_IS_BIGGEST,
+	TIMER,
+} new_event;
 
-typedef struct
+struct skystate
 {
 	int q0;
 	int q1;
 	int q2;
 	int q3;
 	int q4;
-} SkyState;
+} sky_state;
 
-typedef struct
+struct arrayofsensors
 {
 	int z;
 	int x;
 	int y;
-} Scanner;
+} array_of_sensors;
 
 Servo serv_az;
 Servo serv_alt;
-MachineState machine_state;
-SkyState sky_state;
-Scanner scanner;
+unsigned long time_from;
+unsigned long now;
 
 void setup()
 {
-	state = SETUP;
+	machine_state = SETUP;
+
 	Serial.begin(9600);
+
+	pinMode(LED, OUTPUT);
+	digitalWrite(LED, HIGH);
+
 	serv_az.attach(SERV_AZ);
 	serv_alt.attach(SERV_ALT);
+
+	aim_to_Q1_Q3_AXIS();
 }
 
 void loop()
 {
-	Event event = get_event();
+	new_event = get_event();
+	state_table[machine_state][new_event]();
 
 	// Para debugging
 	Serial.print(sun_pos_plane.x);
@@ -83,25 +118,115 @@ void loop()
 // Captura de eventos
 //
 
-Event get_event()
+event get_event()
 {
-	scanner.z = readSensor(LDR_Z);
-	scanner.x = readSensor(LDR_X);
-	scanner.y = readSensor(LDR_Y);
+	if (digitalRead(LED) == LOW)
+	{
+		now = millis();
+		if (now - time_from < MAX_TIME)
+			return TIMER;
+	}
 
-	sky_state.q0 = scanner.z;
+	array_of_sensors.z = readSensor(LDR_Z);
+	array_of_sensors.x = readSensor(LDR_X);
+	array_of_sensors.y = readSensor(LDR_Y);
+
+	sky_state.q0 = array_of_sensors.z;
 	if (serv_az.read() == 0)
 	{
-		sky_state.q1 = scanner.x;
-		sky_state.q3 = scanner.y;
-		return PASS;
+		sky_state.q1 = array_of_sensors.x;
+		sky_state.q3 = array_of_sensors.y;
+		return CONTINUE;
 	}
 	else // serv_az.read() == 90
 	{
-		sky_state.q2 = scanner.x;
-		sky_state.q4 = scanner.y;
-		// TODO: Ver qué estado Qn_IS_BIGGEST devolver
+		sky_state.q2 = array_of_sensors.x;
+		sky_state.q4 = array_of_sensors.y;
+		return biggest_qn();
 	}
+}
 
-	// TODO: Timer
+event biggest_qn()
+{
+	event max_q = Q0_IS_BIGGEST;
+	int max_value = sky_state.q0;
+	if (max_value < sky_state.q1)
+	{
+		max_q = Q1_IS_BIGGEST;
+		max_value = sky_state.q1;
+	}
+	if (max_value < sky_state.q2)
+	{
+		max_q = Q2_IS_BIGGEST;
+		max_value = sky_state.q2;
+	}
+	if (max_value < sky_state.q3)
+	{
+		max_q = Q3_IS_BIGGEST;
+		max_value = sky_state.q3
+	}
+	if (max_value < sky_state.q4)
+	{
+		max_q = Q4_IS_BIGGEST;
+	}
+	return max_q;
+}
+
+// Apuntado
+//
+
+void aim_to_Q1_Q3_AXIS()
+{
+	serv_az.write(0);
+	serv_alt.write(0);
+	machine_state = ON_Q1_Q3_AXIS;
+}
+
+void aim_to_Q2_Q4_AXIS()
+{
+	serv_az.write(90);
+	serv_alt.write(0);
+	machine_state = ON_Q2_Q4_AXIS;
+}
+
+void aim_to_Q0()
+{
+	serv_az.write(0);
+	serv_alt.write(0);
+	machine_state = ON_Q0;
+}
+
+void aim_to_Q1()
+{
+	serv_az.write(0);
+	serv_alt.write(45);
+	machine_state = ON_Q1;
+}
+
+void aim_to_Q2()
+{
+	serv_az.write(90);
+	serv_alt.write(135);
+	machine_state = ON_Q2;
+}
+
+void aim_to_Q3()
+{
+	serv_az.write(0);
+	serv_alt.write(135);
+	machine_state = ON_Q3;
+}
+
+void aim_to_Q4()
+{
+	serv_az.write(90);
+	serv_alt.write(45);
+	machine_state = ON_Q4;
+}
+
+// Error
+//
+void error()
+{
+	return;
 }
