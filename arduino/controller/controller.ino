b17a2677418dba3166e0f41d328002c2ddfd78b2
Pablo Ferreira <pablomartinferre@gmail.com>
2024-04-23 11:56:05 -0300

    Nuea versión solo con tres posiciones y sin timers

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -3,22 +3,18 @@
 // Constants
 //
 
-#define MAX_TIME 10000 // 10s
 #define ZENITH 90
-#define ZERO 0
-#define QUARTER 90
-#define LEAN_45 45
-#define LEAN_135 135
+#define EAST 40
+#define WEST 140
+#define DEVIATION 200 // Cota para que cambie el estado al cambiar LDRs
 
 // Pin map
 //
 
-#define LDR_Z A0
-#define LDR_X A1
-#define LDR_Y A2
+#define LDR0 A0
+#define LDR1 A1
 
 #define LED 2
-#define SERV_AZ 3
 #define SERV_ALT 5
 
 // Data types
@@ -27,78 +23,50 @@
 enum machinestate
 {
 	SETUP,
-	ON_Q1_Q3_AXIS,
-	ON_Q2_Q4_AXIS,
-	ON_Q0,
-	ON_Q1,
-	ON_Q2,
-	ON_Q3,
-	ON_Q4,
+	AIM_ZENITH,
+	AIM_EAST,
+	AIM_WEST,
 } machine_state;
 
 enum event
 {
 	CONTINUE,
-	Q0_IS_BIGGEST,
-	Q1_IS_BIGGEST,
-	Q2_IS_BIGGEST,
-	Q3_IS_BIGGEST,
-	Q4_IS_BIGGEST,
-	WAKE_UP,
+	EAST_IS_BIGGER,
+	WEST_IS_BIGGER,
+	RESET,
 } new_event;
-
-struct skystate
-{
-	int q0;
-	int q1;
-	int q2;
-	int q3;
-	int q4;
-} sky_state;
 
 struct arrayofsensors
 {
-	int z;
-	int x;
-	int y;
+	int east;
+	int west;
 } array_of_sensors;
 
-Servo serv_az;
 Servo serv_alt;
-unsigned long time_from;
-unsigned long now;
 
 // Prototypes
 //
 
-event biggest_qn();
 event get_event();
-void aim_to_Q1_Q3_AXIS();
-void aim_to_Q2_Q4_AXIS();
-void aim_to_Q0();
-void aim_to_Q1();
-void aim_to_Q2();
-void aim_to_Q3();
-void aim_to_Q4();
+event calculate_event();
+void aim_to_zenith();
+void aim_to_east();
+void aim_to_west();
 void error();
 
 // Matrix
 //
 
-#define NUM_OF_STATES 8
-#define NUM_OF_EVENTS 7
+#define NUM_OF_STATES 4
+#define NUM_OF_EVENTS 4
 
 typedef void (*transition)();
 transition state_table[NUM_OF_STATES][NUM_OF_EVENTS] =
 	{
-		{error, error, error, error, error, error, error},						  // state SETUP
-		{aim_to_Q2_Q4_AXIS, error, error, error, error, error, error},			  // state ON_Q1_Q3_AXIS
-		{sleeping, aim_to_Q0, aim_to_Q1, aim_to_Q2, aim_to_Q3, aim_to_Q4, error}, // state ON_Q2_Q4_AXIS
-		{sleeping, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		  // state ON_Q0
-		{sleeping, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		  // state ON_Q1
-		{sleeping, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		  // state ON_Q2
-		{sleeping, error, error, error, error, error, aim_to_Q1_Q3_AXIS},		  // state ON_Q3
-		{sleeping, error, error, error, error, error, aim_to_Q1_Q3_AXIS}		  // state ON_Q4
+		{cont, error, error, reset},			 // state SETUP
+		{cont, aim_to_east, aim_to_west, reset}, // state AIM_ZENITH
+		{cont, cont, aim_to_zenith, reset},		 // state AIM_EAST
+		{cont, aim_to_zenith, cont, reset},		 // state AIM_WEST
 
 };
 
@@ -114,20 +82,15 @@ void setup()
 	pinMode(LED, OUTPUT);
 	digitalWrite(LED, HIGH);
 
-	serv_az.attach(SERV_AZ);
 	serv_alt.attach(SERV_ALT);
 
-	aim_to_Q1_Q3_AXIS();
-	delay(1000);
+	aim_to_zenith();
 }
 
 void loop()
 {
 	new_event = get_event();
 	state_table[machine_state][new_event]();
-	Serial.println(new_event);
-
-	delay(1000);
 }
 
 // Captura de eventos
@@ -135,128 +98,55 @@ void loop()
 
 event get_event()
 {
-	if (digitalRead(LED) == LOW)
-	{
-		now = millis();
-		if (now - time_from >= MAX_TIME)
-			return WAKE_UP;
-		else
-			return CONTINUE;
-	}
+	// if (digitalRead(LED) == LOW)
+	// {
+	// 	now = millis();
+	// 	if (now - time_from >= MAX_TIME)
+	// 		return WAKE_UP;
+	// 	else
+	// 		return CONTINUE;
+	// }
 
-	array_of_sensors.z = analogRead(LDR_Z);
-	array_of_sensors.x = analogRead(LDR_X);
-	array_of_sensors.y = analogRead(LDR_Y);
-
-	sky_state.q0 = array_of_sensors.z;
-	if (serv_az.read() == ZERO)
-	{
-		sky_state.q1 = array_of_sensors.x;
-		sky_state.q3 = array_of_sensors.y;
-		return CONTINUE;
-	}
-	else // serv_az.read() == 90, etc
-	{
-		sky_state.q2 = array_of_sensors.x;
-		sky_state.q4 = array_of_sensors.y;
-		return biggest_qn();
-	}
+	array_of_sensors.east = analogRead(LDR0);
+	array_of_sensors.west = analogRead(LDR1);
+	return calculate_event();
 }
 
-event biggest_qn()
+event calculate_event()
 {
-	event max_q = Q0_IS_BIGGEST;
-	int max_value = sky_state.q0;
-	if (max_value < sky_state.q1)
-	{
-		max_q = Q1_IS_BIGGEST;
-		max_value = sky_state.q1;
-	}
-	if (max_value < sky_state.q2)
-	{
-		max_q = Q2_IS_BIGGEST;
-		max_value = sky_state.q2;
-	}
-	if (max_value < sky_state.q3)
-	{
-		max_q = Q3_IS_BIGGEST;
-		max_value = sky_state.q3;
-	}
-	if (max_value < sky_state.q4)
-	{
-		max_q = Q4_IS_BIGGEST;
-	}
-	return max_q;
+	if (array_of_sensors.east <= array_of_sensors.west * (1 + DEVIATION) && array_of_sensors.east >= array_of_sensors.west * (1 - DEVIATION))
+		return CONTINUE;
+	else if (array_of_sensors.east > array_of_sensors.west)
+		return EAST_IS_BIGGER;
+	else
+		return WEST_IS_BIGGER;
 }
 
 // Apuntado
 //
 
-void aim_to_Q1_Q3_AXIS()
+void aim_to_zenith()
 {
-	serv_az.write(ZERO);
 	serv_alt.write(ZENITH);
-	digitalWrite(LED, HIGH);
-	machine_state = ON_Q1_Q3_AXIS;
-	Serial.println("ON_Q1_Q3_AXIS");
-}
-
-void aim_to_Q2_Q4_AXIS()
-{
-	serv_az.write(QUARTER);
-	serv_alt.write(ZENITH);
-	machine_state = ON_Q2_Q4_AXIS;
-	Serial.println("ON_Q2_Q4_AXIS");
-}
-
-void aim_to_Q0()
-{
-	serv_az.write(ZERO); // Opcional
-	serv_alt.write(QUARTER);
 	digitalWrite(LED, LOW);
-	time_from = millis();
-	machine_state = ON_Q0;
-	Serial.println("ON_Q0");
+	machine_state = AIM_ZENITH;
+	Serial.println("AIM_ZENITH");
 }
 
-void aim_to_Q1()
+void aim_to_east()
 {
-	serv_az.write(ZERO);
-	serv_alt.write(LEAN_45);
+	serv_alt.write(EAST);
 	digitalWrite(LED, LOW);
-	time_from = millis();
-	machine_state = ON_Q1;
-	Serial.println("ON_Q1");
+	machine_state = AIM_EAST;
+	Serial.println("AIM_EAST");
 }
 
-void aim_to_Q2()
+void aim_to_west()
 {
-	serv_az.write(QUARTER);
-	serv_alt.write(LEAN_135);
+	serv_alt.write(WEST);
 	digitalWrite(LED, LOW);
-	time_from = millis();
-	machine_state = ON_Q2;
-	Serial.println("ON_Q2");
-}
-
-void aim_to_Q3()
-{
-	serv_az.write(ZERO);
-	serv_alt.write(LEAN_135);
-	digitalWrite(LED, LOW);
-	time_from = millis();
-	machine_state = ON_Q3;
-	Serial.println("ON_Q3");
-}
-
-void aim_to_Q4()
-{
-	serv_az.write(QUARTER);
-	serv_alt.write(LEAN_45);
-	digitalWrite(LED, LOW);
-	time_from = millis();
-	machine_state = ON_Q4;
-	Serial.println("ON_Q4");
+	machine_state = AIM_WEST;
+	Serial.println("AIM_WEST");
 }
 
 // Error
@@ -268,10 +158,20 @@ void error()
 	return;
 }
 
-// Sleeping
+// Continue
+//
 
-void sleeping()
+void cont()
 {
-	Serial.println("SLEEPING");
+	Serial.println("CONTINUE");
+	return;
+}
+
+// Reset
+//
+
+void reset()
+{
+	Serial.println("RESET");
 	return;
 }
