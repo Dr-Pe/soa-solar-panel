6cf33241f16aaea77559fe53bf1e08dc88aa95c2
EmaJuarezDev <emanuel.juarez1999@outlook.com>
2024-04-26 21:35:02 -0300

    New Code Dinamic Solution

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -1,45 +1,34 @@
 #include <Servo.h>
 
-// Constants
-//
-
-#define ZENITH 90
-#define EAST 40
-#define WEST 140
-#define DEVIATION 200 // Cota para que cambie el estado al cambiar LDRs
-
 // Pin map
-//
-
 #define LDR0 A0
 #define LDR1 A1
 
-#define LED 2
+#define LED 4
 #define SERV_ALT 5
 
 // Data types
-//
 
 enum machinestate
 {
 	SETUP,
-	AIM_ZENITH,
-	AIM_EAST,
-	AIM_WEST,
+	BALANCE,
+	GO_SOUTH,
+	GO_NORTH,
 } machine_state;
 
 enum event
 {
-	CONTINUE,
-	EAST_IS_BIGGER,
-	WEST_IS_BIGGER,
-	RESET,
+	SET_SENSOR_FINISHED,
+	LDRS_EQUALS,
+	LDR_SOUTH_BIGGEST,
+	LDR_NORTH_BIGGEST
 } new_event;
 
 struct arrayofsensors
 {
-	int east;
-	int west;
+	int south;
+	int north;
 } array_of_sensors;
 
 Servo serv_alt;
@@ -47,15 +36,20 @@ Servo serv_alt;
 // Prototypes
 //
 
-event get_event();
-event calculate_event();
-void aim_to_zenith();
-void aim_to_east();
-void aim_to_west();
+void get_event();
+void go_south();
+void go_north();
+void cont();
 void error();
+void init_serv();
+void led_off();
+void led_om();
+void move_serv(int);
+void reset_timer();
+void fsm();
+void wait();
 
 // Matrix
-//
 
 #define NUM_OF_STATES 4
 #define NUM_OF_EVENTS 4
@@ -63,115 +57,149 @@ void error();
 typedef void (*transition)();
 transition state_table[NUM_OF_STATES][NUM_OF_EVENTS] =
 	{
-		{cont, error, error, reset},			 // state SETUP
-		{cont, aim_to_east, aim_to_west, reset}, // state AIM_ZENITH
-		{cont, cont, aim_to_zenith, reset},		 // state AIM_EAST
-		{cont, aim_to_zenith, cont, reset},		 // state AIM_WEST
+		{wait, cont, go_south, go_north},  // state SETUP
+		{error, cont, go_south, go_north}, // state BALANCE
+		{error, cont, go_south, go_north}, // state GO_SOUTH
+		{error, cont, go_south, go_north}, // state GO_NORTH
 
 };
 
-// Main
-//
+// parameters
+#define SERV_INIT 90
+#define MAX_TIME 500
+#define ANG_MOVE 5
+#define DIFF_MIN_LDR 100
 
+// globalVar
+unsigned long time_from;
+unsigned long now;
+
+// functions
+void init_serv()
+{
+	serv_alt.write(SERV_INIT);
+}
+
+void led_on()
+{
+	digitalWrite(LED, HIGH);
+}
+
+void led_off()
+{
+	digitalWrite(LED, LOW);
+}
+
+void move_serv(int offset)
+{
+	serv_alt.write(serv_alt.read() + offset);
+}
+
+void reset_timer()
+{
+	time_from = millis();
+}
+
+void cont()
+{
+	led_off();
+	if ((now - time_from) >= MAX_TIME)
+	{
+		reset_timer();
+	}
+	machine_state = BALANCE;
+}
+
+void wait()
+{
+}
+
+void error()
+{
+	// Ini-Debug
+	Serial.print("PROBLEMA");
+	// End-Debug
+}
+
+void go_south()
+{
+	machine_state = GO_SOUTH;
+	led_on();
+	move_serv(-ANG_MOVE);
+	if ((now - time_from) >= MAX_TIME)
+	{
+		reset_timer();
+	}
+}
+
+void go_north()
+{
+	machine_state = GO_NORTH;
+	led_on();
+	move_serv(ANG_MOVE);
+	if ((now - time_from) >= MAX_TIME)
+	{
+		reset_timer();
+	}
+}
+
+void get_event()
+{
+	now = millis();
+
+	if ((now - time_from) >= MAX_TIME)
+	{
+
+		array_of_sensors.south = analogRead(LDR0);
+		array_of_sensors.north = analogRead(LDR1);
+
+		if (abs(array_of_sensors.south - array_of_sensors.north) > DIFF_MIN_LDR)
+		{
+			if (array_of_sensors.south > array_of_sensors.north)
+			{
+				new_event = LDR_SOUTH_BIGGEST;
+			}
+			else
+			{
+				new_event = LDR_NORTH_BIGGEST;
+			}
+		}
+		else
+		{
+			new_event = LDRS_EQUALS;
+		}
+	}
+	else
+	{
+		// new_event = CONTINUE;
+	}
+}
+
+void fsm()
+{
+	while (1)
+	{
+		get_event();
+		state_table[machine_state][new_event]();
+	}
+}
+
+// principal
 void setup()
 {
-	machine_state = SETUP;
-
+	// Ini-Debug
 	Serial.begin(9600);
+	// End-Debug
 
+	machine_state = SETUP;
 	pinMode(LED, OUTPUT);
 	digitalWrite(LED, HIGH);
-
 	serv_alt.attach(SERV_ALT);
-
-	aim_to_zenith();
+	init_serv();
+	reset_timer();
 }
 
 void loop()
 {
-	new_event = get_event();
-	state_table[machine_state][new_event]();
-}
-
-// Captura de eventos
-//
-
-event get_event()
-{
-	// if (digitalRead(LED) == LOW)
-	// {
-	// 	now = millis();
-	// 	if (now - time_from >= MAX_TIME)
-	// 		return WAKE_UP;
-	// 	else
-	// 		return CONTINUE;
-	// }
-
-	array_of_sensors.east = analogRead(LDR0);
-	array_of_sensors.west = analogRead(LDR1);
-	return calculate_event();
-}
-
-event calculate_event()
-{
-	if (array_of_sensors.east <= array_of_sensors.west * (1 + DEVIATION) && array_of_sensors.east >= array_of_sensors.west * (1 - DEVIATION))
-		return CONTINUE;
-	else if (array_of_sensors.east > array_of_sensors.west)
-		return EAST_IS_BIGGER;
-	else
-		return WEST_IS_BIGGER;
-}
-
-// Apuntado
-//
-
-void aim_to_zenith()
-{
-	serv_alt.write(ZENITH);
-	digitalWrite(LED, LOW);
-	machine_state = AIM_ZENITH;
-	Serial.println("AIM_ZENITH");
-}
-
-void aim_to_east()
-{
-	serv_alt.write(EAST);
-	digitalWrite(LED, LOW);
-	machine_state = AIM_EAST;
-	Serial.println("AIM_EAST");
-}
-
-void aim_to_west()
-{
-	serv_alt.write(WEST);
-	digitalWrite(LED, LOW);
-	machine_state = AIM_WEST;
-	Serial.println("AIM_WEST");
-}
-
-// Error
-//
-
-void error()
-{
-	Serial.println("ERROR");
-	return;
-}
-
-// Continue
-//
-
-void cont()
-{
-	Serial.println("CONTINUE");
-	return;
-}
-
-// Reset
-//
-
-void reset()
-{
-	Serial.println("RESET");
-	return;
+	fsm();
 }
